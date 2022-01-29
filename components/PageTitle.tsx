5262cee7e6c9ed89941606122583146a5a138d9d
Deepak Kumar <git.logsbydeepak@gmail.com>
2022-01-29 20:12:29 +0530

    refactor page title

--- components/PageTitle.tsx
+++ components/PageTitle.tsx
@@ -1,20 +1,18 @@
 import { FunctionComponent } from "react";
 
-import pageTitleStyle from "../styles/module/components/pageTitle.module.scss";
+import style from "styles/module/components/pageTitle.module.scss";
 
-type Props = {
+interface Props {
   title: string;
   subtitle: string;
-};
+}
 
 const PageTitle: FunctionComponent<Props> = ({ title, subtitle }) => {
   return (
-    <>
-      <div className={pageTitleStyle.base}>
-        <h1>{title}</h1>
-        <h3>{subtitle}</h3>
-      </div>
-    </>
+    <div className={style.container}>
+      <h1>{title}</h1>
+      <h3>{subtitle}</h3>
+    </div>
   );
 };
 
