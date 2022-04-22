bcdb344316296648c3c9d477c8548e8c6fd80ce6
Deepak Kumar <git.logsbydeepak@gmail.com>
2022-04-22 14:24:37 +0000

    replace page progress bar

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -3,47 +3,33 @@ import type { AppProps } from "next/app";
 
 import "styles/globals.scss";
 import { NotificationProvider } from "context/NotificationContext";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
 import { AuthProvider } from "context/AuthContext";
-import { PageProgress } from "./App/components/progress";
+import NextNProgress from "nextjs-progressbar";
 import Layout from "./App/components/Layout";
 
-const MyApp = ({ Component, pageProps }: AppProps) => {
-  const router = useRouter();
-  const [isPageLoading, setIsPageLoading] = useState(false);
-
-  useEffect(() => {
-    router.events.on("routeChangeStart", () => setIsPageLoading(true));
-    router.events.on("routeChangeComplete", () => setIsPageLoading(false));
-    router.events.on("routeChangeError", () => setIsPageLoading(false));
-
-    return () => {
-      router.events.off("routeChangeStart", () => setIsPageLoading(true));
-      router.events.off("routeChangeComplete", () => setIsPageLoading(false));
-      router.events.off("routeChangeError", () => setIsPageLoading(false));
-    };
-  }, [router]);
-
-  return (
-    <>
-      <Head>
-        <link rel="icon" href="/favicon.png" />
-        <title>TODO</title>
-      </Head>
-      <PageProgress progress={isPageLoading} />
-      <AuthProvider>
-        <NotificationProvider>
-          <div className="container">
-            <Layout>
-              {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-              <Component {...pageProps} />
-            </Layout>
-          </div>
-        </NotificationProvider>
-      </AuthProvider>
-    </>
-  );
-};
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Head>
+      <link rel="icon" href="/favicon.png" />
+      <title>TODO</title>
+    </Head>
+    {/* <PageProgress progress={isPageLoading} /> */}
+    <NextNProgress
+      options={{
+        showSpinner: false,
+      }}
+    />
+    <AuthProvider>
+      <NotificationProvider>
+        <div className="container">
+          <Layout>
+            {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+            <Component {...pageProps} />
+          </Layout>
+        </div>
+      </NotificationProvider>
+    </AuthProvider>
+  </>
+);
 
 export default MyApp;
